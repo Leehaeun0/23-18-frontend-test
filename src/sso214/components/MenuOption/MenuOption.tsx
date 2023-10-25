60ee3b9284446e596501fff309aa3509bd3a1535
sso214 <sojam214@gmail.com>
2023-10-25 21:17:08 +0900

    feat : MenuOption 컴포넌트 구현

---
+++ src/sso214/components/MenuOption/MenuOption.tsx
@@ -0,0 +1,68 @@
+import { MenuInfo } from '../Menu/types';
+import Heading from '../Heading/Heading';
+import { NumberAdjuster } from '../NumberAdjuster';
+
+interface Props {
+  menu: MenuInfo;
+}
+
+const MenuOption = ({ menu }: Props) => {
+  const { name, options, image, description, isPopular } = menu;
+  const isMultiOption = options.length > 1;
+
+  return (
+    <form>
+      {image && <img src={image} alt={name} data-testid="image" />}
+
+      <div>
+        <Heading headingLevel="h3" data-testid="name">
+          {isPopular && <span data-testid="popular">인기</span>}
+          {name}
+        </Heading>
+
+        {description && <p data-testid="description">{description}</p>}
+
+        {!isMultiOption && (
+          <div>
+            <Heading headingLevel="h4">가격</Heading>
+            <Heading headingLevel="h4">${options[0].price}원</Heading>
+          </div>
+        )}
+      </div>
+
+      <ol>
+        {isMultiOption && (
+          <li>
+            <div>
+              <Heading headingLevel="h4">가격</Heading>
+              <span>필수</span>
+            </div>
+
+            <ul>
+              {options?.map((option, key) => (
+                <li key={key}>
+                  <label>
+                    <input type="radio" />
+                    <p>{option?.name}</p>
+                    <p>{option.price}원</p>
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </li>
+        )}
+
+        <li>
+          <p>수량</p>
+          <NumberAdjuster onClick={(num) => console.log(num)} />
+        </li>
+      </ol>
+
+      <div>
+        <button type="submit">00000원 담기</button>
+      </div>
+    </form>
+  );
+};
+
+export default MenuOption;
--- src/sso214/components/index.ts
+++ src/sso214/components/index.ts
@@ -2,3 +2,5 @@ export * from './Heading';
 export * from './List';
 export * from './MenuList';
 export * from './Menu';
+export * from './NumberAdjuster';
+export * from './MenuOption';

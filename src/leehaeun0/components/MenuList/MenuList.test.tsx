b9e8ca06cf3aa0c9e55d8a3860fb5a14400ade9f
iver <iver@kcd.co.kr>
2023-11-21 20:33:54 +0900

    전체 페이지 내에서 heading 태그 레벨 조정

--- src/leehaeun0/components/MenuList/MenuList.test.tsx
+++ src/leehaeun0/components/MenuList/MenuList.test.tsx
@@ -5,7 +5,7 @@ import { MENU_DATA } from '../../mocks/menu';
 describe('MenuList Component', () => {
   it('메뉴 타이틀 랜더링 확인', () => {
     const { getByRole } = render(<MenuList data={MENU_DATA} />);
-    const heading = getByRole('heading');
+    const heading = getByRole('heading', { level: 2 });
 
     expect(heading).toHaveTextContent(MENU_DATA.title);
   });
--- src/leehaeun0/components/MenuList/MenuList.tsx
+++ src/leehaeun0/components/MenuList/MenuList.tsx
@@ -4,7 +4,7 @@ import MenuItem from '../MenuItem/MenuItem';
 export default function MenuList({ data }: { data: MenuListType }) {
   return (
     <main>
-      <h1>{data.title}</h1>
+      <h2>{data.title}</h2>
       <ol>
         {data.menus.map((menu) => (
           <MenuItem key={menu.name} data={menu} />

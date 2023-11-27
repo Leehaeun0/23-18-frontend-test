f4079c9f9f359b1f745543a5ffdabc90584073ac
iver <iver@kcd.co.kr>
2023-11-27 22:27:09 +0900

    wip

---
+++ src/leehaeun0/pages/MenuDetailPage/StorePage.tsx
@@ -0,0 +1,21 @@
+import MenuList from 'src/leehaeun0/components/MenuList/MenuList';
+import { MenuListType } from 'src/leehaeun0/types/menu';
+
+export default function StorePage({ data }: { data: MenuListType }) {
+  return (
+    <main>
+      <MenuList data={data} />
+      <OrderButton count={1} price={3200} />
+    </main>
+  );
+}
+
+function OrderButton({ count, price }: { count: number; price: number }) {
+  return (
+    <button type="button" aria-label="주문하기">
+      <span aria-label="장바구니 개수">{count}</span>
+      <span>주문하기</span>
+      <span aria-label="장바구니 가격">{price.toLocaleString()}원</span>
+    </button>
+  );
+}
